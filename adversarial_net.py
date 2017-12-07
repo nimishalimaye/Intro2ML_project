4782b2e09045cd3bd127e008a5ddfa3634633525
Nimisha Limaye <nsl278@nyu.edu>
2017-12-07 15:17:23 -0500

    Add files via upload
    Added confidence values

--- adversarial_net.py
+++ adversarial_net.py
@@ -7,10 +7,10 @@ import cPickle
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-
+#from pprint import pprint
 srng = RandomStreams()
 
-TRAINING = False
+TRAINING = True
 
 # Convert into correct type for theano
 def floatX(X):
@@ -82,19 +82,26 @@ noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
 h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
 y_x = T.argmax(py_x, axis=1)
 
+# To find confidence of test set use the following value of y_x
+y_x1 = T.max(py_x, axis = 1)
 # Define cost and update theano expressions
 
 l1 = abs(w_h).sum() + abs(w_h2).sum() + abs(w_o).sum()
 l2 = (w_h**2).sum() + (w_h2**2).sum() + (w_o**2).sum()
-l1coef = 0.0001
-l2coef = 0.0001
+
+#=================== Parameters to chnge ===============================#
+l1coef = 0.0001 #changed from 0.0001 to 0.01 NL1206 
+l2coef = 0.0001 #changed from 0.0001 to 0.01 NL1206
+#=======================================================================#
+
+
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y)) + l1coef * l1 + l2coef * l2
 updates = RMSprop(cost, params, lr=0.001)
 
 # Define train and predict theano functions
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
-
+predict_conf = theano.function(inputs=[X], outputs=y_x1, allow_input_downcast=True)
 if TRAINING:
     # Train in 50 epochs
     for i in range(50):
@@ -109,6 +116,7 @@ if TRAINING:
         with open('LearnedParamsL1.model','wb') as fp:
             cPickle.dump(params,fp)
 
+#=========================== ADVERSARIAL COMPONENT OF THE CODE ============================#
             
 #____________________________________________________________________________________________
 #
@@ -118,15 +126,62 @@ if TRAINING:
 # Theano function which calculates gradient of the cost function w.r.t. input image
 cost_ad = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
 get_grad = theano.function(inputs=[X, Y], outputs=T.grad(cost_ad, X), allow_input_downcast=True)
-   
+ 
+
+
+def plot_mnist_digit(image1, image2, name1, name2):
+    global count_attack
+    image1 = np.reshape(image1,[1,784])
+    image2 = np.reshape(image2,[1,784])
+    #print 'test image confidence' , np.mean(predict_conf(image1)), 'adversarial image confidence', np.mean(predict_conf(image2))
+    if (predict(image1) != predict(image2)):
+        count_attack = count_attack + 1
+
+
+# Obtain output array with correct test outputs
+Yts = []
+for i in range(len(teY)):
+    for j in range(10):
+        if(teY[i,j] == 1):
+            Yts.append(j)
+
+#============================Parameter to change ========================================#
+eps_values = [0.12, 0.15, 0.17, 0.20]  
+#========================================================================================#
+
+for EPS in eps_values:
+    eps = EPS
+    adX = []
+    for i in range(len(teX)):
+        gs = get_grad(teX[i:i+1], teY[i:i+1]).T[:,0]
+        img_ad = teX[i] + eps * np.sign(gs) 
+        adX.append(img_ad)
+
+   # Find accuracy of the classifier on the test set and adversarial set
+    pred_teY = predict(teX)
+    print 'Test set Accuracy:					', np.mean(np.argmax(teY, axis=1) == predict(teX)) 
+    print 'Adversarial set Accuracy, e=', eps, ':					', np.mean(np.argmax(teY, axis=1) == predict(adX))		#np.mean(predict(adX)!=Yts)
+    print 'Test Set Confidence:					', np.mean(predict_conf(teX))
+    print 'Adversarial set Confidence:				', np.mean(predict_conf(adX))
+    count_attack = 0
+    for i in range(len(teX)):     
+       plot_mnist_digit(teX[i], adX[i], 'test_img{0}.jpg'.format(i),'less_ad_img{0}.jpg'.format(i))
+
+    print 'Percent of successful adversarial attack:{0:3f}:					' .format(float(count_attack*100)/len(teX))
+    print '======================================================================================'
+
+
+
+
+'''
 try:
-    with open('MNIST_test_adversarial_010.bin', 'rb') as fp:
+    with open('MNIST_test_adversarial_0007.bin', 'rb') as fp:
         adX = cPickle.load(fp)
 except IOError: 
     print 'Adversarial examples not found, generating them now...' 
     # Eps is a parameter for strength of the added noise. 
     # Since MNIST dataset is binary, 0.25 error should be 'bellow' the resolution
-    eps = 0.10
+    eps = 0.007
     adX = []
     for i in range(len(teX)):
         if (i % 1000 == 0):
@@ -134,20 +189,20 @@ except IOError:
         gs = get_grad(teX[i:i+1], teY[i:i+1]).T[:,0]
         img_ad = teX[i] + eps * np.sign(gs) 
         adX.append(img_ad)
-    with open('MNIST_test_adversarial_010.bin', 'wb') as fp:
+    with open('MNIST_test_adversarial_0007.bin', 'wb') as fp:
        cPickle.dump(adX, fp)
     
 # Find accuracy of the classifier on the test set and adversarial set
 print 'Test set:                ', np.mean(predict(teX))
-print 'Adversarial set, e=0.10: ', np.mean(predict(adX))
-    
+print 'Adversarial set, e=0.007: ', np.mean(predict(adX))
+ 
 try:
     with open('MNIST_test_adversarial_025.bin', 'rb') as fp:
         adX = cPickle.load(fp)
 except IOError: 
     print 'The other file not found' 
 print 'Adversarial set, e=0.25: ', np.mean(predict(adX))
-    
+ 
 # Let us display some images from both 
 
 # Function for plotting a single MNIST image
@@ -163,20 +218,31 @@ def plot_mnist_digit(image1, image2, name1, name2):
         #print(name1,predict(image1))
         #print(name2,predict(image2))
         count_attack = count_attack + 1
-    #image = np.reshape(image, [28, 28])
-    #fig = plt.figure()
-    #ax = fig.add_subplot(1, 1, 1)
-    #ax.matshow(image, cmap=plt.cm.gray)
-    #plt.xticks(np.array([]))
-    #plt.yticks(np.array([]))
 
+    image1 = np.reshape(image1, [28, 28])
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(image1, cmap=plt.cm.gray)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.savefig(name1)
+    image2 = np.reshape(image2, [28, 28])
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(image2, cmap=plt.cm.gray)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.savefig(name2)
 
 for i in range(len(teX)):     
     #plot_mnist_digit(teX[i], 'test{0}.jpg'.format(i))
     #plot_mnist_digit(adX[i], 'less_ad{0}.jpg'.format(i))
-    plot_mnist_digit(teX[i], adX[i], 'test{0}.jpg'.format(i),'less_ad{0}.jpg'.format(i))
+    plot_mnist_digit(teX[i], adX[i], 'test_img{0}.jpg'.format(i),'less_ad_img{0}.jpg'.format(i))
 
 print 'Percent of successful adversarial attack:{0:3f}	'.format(float(count_attack*100)/len(teX))
+
+'''
+'''
 # Generate random noise examples
 eps = 0.25
 rdX = []
@@ -193,7 +259,7 @@ for i in range(len(teX)):
     #plot_mnist_digit(rdX[i], 'rd{0}.jpg'.format(i))
    
 #print 'Random noise set, e=0.25:', np.mean(predict(rdX))
-
+'''
     
     
     
